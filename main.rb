8f0b236be9f546d1728e49dd8c4f43ac557ca3cc
ItsKatato <kaelahsmallwood@gmail.com>
2022-03-11 10:53:47 -0600

    Added board class and place marker method

--- main.rb
+++ main.rb
@@ -24,14 +24,43 @@ class Player
         puts "Okay #{self.name}, your marker is #{self.marker}."
     end
 
+    def info()
+        return "Player ##{self.player_num}'s name is #{self.name} and has the marker #{self.marker}."
+    end
+
 end
+
+class Board 
+    @@game_board = [[1, 2, 3],[4, 5, 6],[7, 8, 9]]
+    @@ROW_SEP = '-+-+-'
+
+    def initialize()
+    end
+
+    def self.show_board()
+        puts @@game_board[0].join('|')
+        puts @@ROW_SEP
+        puts @@game_board[1].join('|')
+        puts @@ROW_SEP
+        puts @@game_board[2].join('|')
+    end
+
+    def self.place_marker(space, player_marker)
+        @@game_board[@@game_board.find_index {|i| i.include?(space)}][@@game_board.find_index {|i| i.include?(space)}] = player_marker
+    end
+end
+
+# @@game_board[:r1][]
 
 player_1 = Player.new("player_1", "x", 1)
 player_2 = Player.new("player_2", "o", 2)
 
-board = Array.new(9)
 
+# puts "Welcome to Tik-Tac-Toe! What's is your name?"
+# puts player_1.name + ", what's is your name?"
+# player_1.customization()
+# puts player_2.name + ", what's is your name?"
+# player_2.customization()
 
-puts "Welcome to Tik-Tac-Toe! What's is your name?"
-puts player_1.name + ", what's is your name?"
-player_1.customization()
+Board.place_marker(5, player_1.marker)
+Board.show_board()
