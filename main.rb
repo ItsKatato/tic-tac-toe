4dcc683bcca14f85dfa18b48ef4084251602ee67
ItsKatato <kaelahsmallwood@gmail.com>
2022-03-11 16:42:55 -0600

    Created TikTacToe Module

--- main.rb
+++ main.rb
@@ -1,3 +1,53 @@
+module TikTacToe
+
+    def self.player_play(player_obj)
+        Board.show_board
+        puts player_obj.name + ", enter a number (1-9) that is not already taken by a player marker."
+        space_sel = gets.chomp
+        until space_sel.to_i < 10 && space_sel.to_i > 0
+            puts "Please enter a number (1-9)."
+            space_sel = gets.chomp
+        end
+        until Board.place_marker(space_sel, player_obj.marker)
+            puts "Please enter a number (1-9) that is not already taken."
+            space_sel = gets.chomp
+        end
+    end
+
+    def self.gameplay()
+        puts "Welcome to Tik-Tac-Toe! What's is your name?"
+        puts $player_1.name + ", what's is your name?"
+        $player_1.customization()
+        puts ""
+        puts $player_2.name + ", what's is your name?"
+        $player_2.customization()
+        puts ""
+        loop do
+            puts ""
+            player_play($player_1)
+            if Board.match?()
+                break
+            end
+            puts ""
+            player_play($player_2)
+            if Board.match?()
+                break
+            end
+        end
+        puts ""
+        Board.show_board()
+        if Board.winner_marker == $player_1.marker
+            puts "#{$player_1.name} is the winner!"
+        elsif Board.winner_marker == $player_2.marker
+            puts "#{$player_1.name} is the winner!"
+        else
+            puts "Tied Game!"
+        end
+    end
+    
+    
+end
+
 class Player
     attr_accessor :name, :marker
     attr_reader :player_num
@@ -87,56 +137,8 @@ class Board
 
 end
 
-player_1 = Player.new("player_1", "x", 1)
-player_2 = Player.new("player_2", "o", 2)
+$player_1 = Player.new("player_1", "x", 1)
+$player_2 = Player.new("player_2", "o", 2)
 
 
-puts "Welcome to Tik-Tac-Toe! What's is your name?"
-puts player_1.name + ", what's is your name?"
-player_1.customization()
-puts ""
-puts player_2.name + ", what's is your name?"
-player_2.customization()
-puts ""
-loop do
-    puts ""
-    Board.show_board
-    puts player_1.name + ", enter a number (1-9) that is not already taken by a player marker."
-    space_sel = gets.chomp
-    until space_sel.to_i < 10 && space_sel.to_i > 0
-        puts "Please enter a number (1-9)."
-        space_sel = gets.chomp
-    end
-    until Board.place_marker(space_sel, player_1.marker)
-        puts "Please enter a number (1-9) that is not already taken."
-        space_sel = gets.chomp
-    end
-    if Board.match?()
-        break
-    end
-    puts ""
-    Board.show_board
-    puts player_2.name + ", enter a number (1-9) that is not already taken by a player marker."
-    space_sel2 = gets.chomp
-    until space_sel2.to_i < 10 && space_sel2.to_i > 0
-        puts "Please enter a number (1-9)."
-        space_sel2 = gets.chomp
-    end
-    until Board.place_marker(space_sel2, player_2.marker)
-        puts "Please enter a number (1-9) that is not already taken."
-        space_sel2 = gets.chomp
-    end
-    if Board.match?()
-        break
-    end
-end
-
-puts ""
-Board.show_board()
-if Board.winner_marker == player_1.marker
-    puts "#{player_1.name} is the winner!"
-elsif Board.winner_marker == player_2.marker
-    puts "#{player_1.name} is the winner!"
-else
-    puts "Tied Game!"
-end
+TikTacToe.gameplay
